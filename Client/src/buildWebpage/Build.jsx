58fc4dce72dc6fcf394b7d5af8e78f5c3ebe49b0
Nikhil Kumar <nikhil.pmsnick@gmail.com>
2025-01-09 08:44:13 +0530

    created empty components of build page and aligned them

--- Client/src/buildWebpage/Build.jsx
+++ Client/src/buildWebpage/Build.jsx
@@ -1,11 +1,40 @@
-import React from "react"
+  import React from "react"
+  import Header from "./Header";
+  import SideBar from "./SideBar";
+  import WebsiteRenderedPage from "./WebsiteRenderedPage";
 
-const Build = (props) => {
-  return (
-    <div>
-      Build
-    </div>
-  )
-};
+  const Build = (props) => {
+      /**
+     * TODO
+     * -make it as the picture is  provided
+     * -apply proper margin padding and positioning of the component (dont use css if present remove it)
+     * -make it as responsive as possible
+     * -test it on your benchmark to see if it is meeting expectations and then push it and raise pr
+     */
+    console.log("in this page")
+    return (
+      <div style={{ display: "flex", flexDirection: "column", height: "100vh" }}>
+        <div style={{ width: "100%", height: "10%", backgroundColor: "lightgray" }}>
+          <Header />
+        </div>
+        <div style={{ display: "flex", flex: 1 }}>
+          <div
+            style={{
+              width: "10%",
+              backgroundColor: "lightblue",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <SideBar />
+          </div>
+          <div style={{ flex: 1, backgroundColor: "lightgreen", padding: "10px" }}>
+            <WebsiteRenderedPage />
+          </div>
+        </div>
+      </div>
+    )
+  };
 
-export default Build;
+  export default Build;
